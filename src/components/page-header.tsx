d008bdcb06bb3657b57a0db6337f977955581f33
GAURAV KUMAR <gauravk8271@gmail.com>
2025-11-27 21:26:00 +0000

    For kgp logo use the image size don't keep a fixed image size

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -14,6 +14,7 @@ export function PageHeader() {
             width={80}
             height={80}
             data-ai-hint={logo.imageHint}
+            className="h-auto w-auto"
           />
         )}
         <div>
